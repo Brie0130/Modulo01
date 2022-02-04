e936fef7a978f30013a34da84bf3b9c8fe6dd935
Brie0130 <brendabarrosnascimento@gmail.com>
2022-02-03 23:37:31 -0300

    innitial commit

---
+++ projetofinal.js
@@ -0,0 +1,18 @@
+/*
+//Quem é o personagem?
+- Um ocultista 
+//Quais os seus desafios?
+- Conseguir descobrir o que está afetando o paranormal
+//Que opções de escolha ele pode ter a cada período?
+-------- De manhã --------
+    - Se drigir a sede da ordem
+    - Ir direto ao local do avistamento paranormal
+    - Dormir mais um pouco (se atrasa 3h e vai direto ao local do avistamento)
+
+-------- De tarde (no local) --------
+    - Investigar 
+    - Perguntar
+            - Ataque de zumbi de sangue
+//Como essas escolhas vão impactar na história e nas possibilidades de escolha futuras?
+//Como você irá controlar o passar do tempo?
+*/
